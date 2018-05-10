78fddbaf5039c90a890944a65af6ee9e5faf0203
David Krasniy <david.krasniy@calpers.ca.gov>
2018-05-10 13:13:38 -0700

    Added dynamic ID

--- src/App.js
+++ src/App.js
@@ -38,7 +38,31 @@ class App extends Component {
         </DropdownMenu>
       </Dropdown>
 
-        <PersonData/>
+      <select>
+  <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="mercedes">Mercedes</option>
+  <option value="audi">Audi</option>
+</select>
+
+
+        <PersonData calpersid="1677401305"/>
+        <p>
+        5600493427
+6474994805
+6452522440
+5564535492
+6632408185
+4618604679
+5826752269
+3996179678
+7302651964
+1677401305
+6827859055
+3685610572
+6581985123
+3148148090
+</p>
 
 
       </div>
--- src/components/PersonData.js
+++ src/components/PersonData.js
@@ -3,14 +3,18 @@ import React, { Component } from 'react';
 
 export default class PersonData extends Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
     this.state = {
         userData: {},
     }
 }
-componentWillMount() {
-  fetch("http://ws-timo2-mycalpers.calpers.ca.gov/env08ws/restservices-interface/participant?cid=2688620634")
+
+CALPERSID = this.props.calpersid;
+
+componentDidMount() {
+  fetch("http://ws-timo2-mycalpers.calpers.ca.gov/env08ws/restservices-interface/participant?cid="+this.CALPERSID)
       .then((response) => response.json())
       .then((responseData) => {
           this.setState({userData: responseData[0]});
@@ -23,12 +27,15 @@ componentWillMount() {
 
 
     render() {
+      console.log(this.props);
       return (
      <div>
        <h1>Welcome back, {this.state.userData.firstName}</h1>
-       <p>CalPERS ID: 90390290490</p>
-       </div>
-
+       <p>First Name: {this.state.userData.firstName}</p>
+       <p>Last Name: {this.state.userData.lastName}</p>
+       <p>Membership Date: {this.state.userData.membershipDate}</p>
+       <p>First Name: {this.state.userData.firstName}</p>
+             </div>
       );
     }
   }
